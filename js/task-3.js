7951731339b0ec86fbccbe758fcff7bce5eff3b7
akmetjuk <akmetjuk@gmail.com>
2026-03-23 22:44:13 +0200

    complete all tasks

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  let retArray = []
+  const condition = Number(value)
+
+  for (let n of numbers)
+    if (n > condition) retArray.push(n);
+
+  return retArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
